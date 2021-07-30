709dec520bbf2f5e1e7e94b3537bcd5161815da7
Demilululu <demilululu@gmail.com>
2021-07-30 21:04:16 +0800

    Initialise project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+
+const PORT = 3000
+
+const app = express()
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine','hbs')
+app.use(express.static(public))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+app.listen(PORT, () => {
+  console.log(` App is now listening on https://localhost:${PORT}`)
+})
+
