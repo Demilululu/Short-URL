1763513078e568812952dc9c3c5bb9976be1216c
Demilululu <demilululu@gmail.com>
2021-07-30 23:09:50 +0800

    Add index.hbs and main.css

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
 
 require('./config/mongoose')
 const routes = require('./routes/index')
@@ -10,9 +9,8 @@ const app = express()
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine','hbs')
-app.use(express.static(public))
+app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(PORT, () => {
